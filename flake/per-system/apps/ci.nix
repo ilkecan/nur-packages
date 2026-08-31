ab839e966a0753067577fde881ede0c37e83acb0
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-08-31 19:41:55 +0300

    update README

--- flake/per-system/apps/ci.nix
+++ flake/per-system/apps/ci.nix
@@ -8,6 +8,7 @@ pkgs.writeShellApplication {
   runtimeInputs = with pkgs; [ nix-fast-build ];
 
   text = ''
+    # `--impure` is needed for `NIXPKGS_ALLOW_UNFREE=1`
     NIXPKGS_ALLOW_UNFREE=1 nix-fast-build --impure --no-nom --skip-cached --cachix-cache ilkecan "$@"
   '';
 }
