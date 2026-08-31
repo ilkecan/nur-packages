ba767fbe493d89b27e6c4360b21eecfff6f20ca6
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-08-31 16:28:28 +0300

    flake/checks: filter unavailable and unbuildable derivations

--- flake/per-system/checks.nix
+++ flake/per-system/checks.nix
@@ -10,8 +10,16 @@
       config,
       lib',
       pkgs,
+      system,
       ...
     }:
+    let
+      ciPackages = lib.filterAttrs (
+        _: package:
+        lib.meta.availableOn { inherit system; } package
+        && lib.elem system (package.meta.hydraPlatforms or [ system ])
+      ) config.packages;
+    in
     {
       checks = {
         health-check = import ./checks/health-check.nix {
@@ -24,6 +32,6 @@
             ;
         };
       }
-      // lib.mapAttrs' (name: lib.nameValuePair "${name}-package") config.packages;
+      // lib.mapAttrs' (name: lib.nameValuePair "${name}-package") ciPackages;
     };
 }
